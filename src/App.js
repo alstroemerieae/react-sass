9cf57a0507f7e54e811a18e244bdfa92ff0cde72
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-04 12:05:46 -0300

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
+import Home from './components/Home';
 import './sass/App.scss';
-import Header from './components/Header';
-import Subheader from './components/Subheader';
-import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Subheader/>
-      <Projects/>
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Subheader from '../components/Subheader';
+import Projects from '../components/Projects';
+
+const Home = () => {
+  return (
+    <div className="App">
+      <Header/>
+      <Subheader/>
+      <Projects/>
+    </div>
+  );
+}
+
+export default Home;
