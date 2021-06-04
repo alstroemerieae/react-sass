8983cf8d89a7c318b66c30307fe86aa45b387d97
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-04 12:03:10 -0300

    Add projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,97 @@
+import image from '../assets/Ellipse 46.png'
+import vector from '../assets/Vector.svg'
+
+const Projects = () => {
+  return (
+    <div className="App-projects">
+      <div className="App-projects-list">
+        {/* Project #1 */}
+        <div className="App-project">
+          {/* Project details section */}
+          <div className="App-project--details">
+            <div className="App-project--details__name">
+              Landing page
+          </div>
+            <div className="App-project--details__creation-date">
+              Creation date:  09/09/2020 10:30 am
+          </div>
+          </div>
+          {/* Project user section */}
+          <div className="App-project--user">
+            <div className="App-project--user__image">
+              <img src={image} alt="Project user name"></img>
+            </div>
+            <div className="App-project--user__name">
+              Ignacio Truffa
+          </div>
+          </div>
+          {/* Project menu icon */}
+          <div className="App-project--menu">
+            <div className="App-project--menu__icon">
+              <img src={vector} alt="Menu icon" />
+            </div>
+          </div>
+        </div>
+        {/* Project #1 End */}
+        {/* Project #2 */}
+        <div className="App-project">
+          {/* Project details section */}
+          <div className="App-project--details">
+            <div className="App-project--details__name">
+              E-Commerce Shop
+          </div>
+            <div className="App-project--details__creation-date">
+              Creation date:  09/09/2020 10:30 am
+          </div>
+          </div>
+          {/* Project user section */}
+          <div className="App-project--user">
+            <div className="App-project--user__image">
+              <img src={image} alt="Project user name"></img>
+            </div>
+            <div className="App-project--user__name">
+              Ignacio Truffa
+          </div>
+          </div>
+          {/* Project menu icon */}
+          <div className="App-project--menu">
+            <div className="App-project--menu__icon">
+              <img src={vector} alt="Menu icon" />
+            </div>
+          </div>
+        </div>
+        {/* Project #2 End */}
+        {/* Project #3 */}
+        <div className="App-project">
+          {/* Project details section */}
+          <div className="App-project--details">
+            <div className="App-project--details__name">
+              CRM Linkroom
+          </div>
+            <div className="App-project--details__creation-date">
+              Creation date:  09/09/2020 10:30 am
+          </div>
+          </div>
+          {/* Project user section */}
+          <div className="App-project--user">
+            <div className="App-project--user__image">
+              <img src={image} alt="Project user name"></img>
+            </div>
+            <div className="App-project--user__name">
+              Ignacio Truffa
+          </div>
+          </div>
+          {/* Project menu icon */}
+          <div className="App-project--menu">
+            <div className="App-project--menu__icon">
+              <img src={vector} alt="Menu icon" />
+            </div>
+          </div>
+        </div>
+        {/* Project #3 End */}
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
