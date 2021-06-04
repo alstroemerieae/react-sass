f3ffb7ce5d9c709f717a0b5b746036651beca020
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-04 13:50:59 -0300

    Add EditProject component

---
+++ src/components/EditProject.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Subheader from '../components/Subheader';
+import ModifyProject from './ModifyProject';
+
+const AddProject = () => {
+  return (
+    <div className="App">
+      <Header/>
+      <Subheader/>
+      <ModifyProject/>
+    </div>
+  );
+}
+
+export default AddProject;
