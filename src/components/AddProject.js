c7badff4ba9ec797f0724db5906fe56bb76794f3
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-04 12:31:51 -0300

    Add AddProject component

---
+++ src/components/AddProject.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Subheader from '../components/Subheader';
+import NewProject from './NewProject';
+
+const AddProject = () => {
+  return (
+    <div className="App">
+      <Header/>
+      <Subheader/>
+      <NewProject/>
+    </div>
+  );
+}
+
+export default AddProject;
