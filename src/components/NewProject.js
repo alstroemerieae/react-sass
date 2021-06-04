fbedc12d54d4f8d41b01b52bd9a4432d4592e36e
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-04 12:30:55 -0300

    Add NewProject component

---
+++ src/components/NewProject.js
@@ -0,0 +1,12 @@
+const NewProject = () => {
+  return (
+    <div className="App-new-project">
+      <form>
+        <label htmlFor="test">Test</label>
+        <input type="test" />
+      </form>
+    </div>
+  );
+}
+
+export default NewProject;
