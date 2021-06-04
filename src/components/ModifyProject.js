bf81f1d3fb00809af5da0e6174cdb0da5bcea55b
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-04 20:01:35 -0300

    Add validation to forms

--- src/components/ModifyProject.js
+++ src/components/ModifyProject.js
@@ -7,65 +7,89 @@ import useFetch from '../hooks/useFetch'
 const ModifyProject = () => {
   const { id } = useParams();
   const { error } = useFetch('http://localhost:8000/projects/' + id);
-
-  const [projectname, setProjectName] = useState('');
-  const [projectdescription, setProjectDescription] = useState('');
-  const [projectdate] = useState('Creation date:  09/09/2020 10:30 am');
-  const [projectmanager, setProjectManager] = useState('');
-  const [projectdeveloper, setProjectDeverloper] = useState('');
-  const [projectstatus, setProjectStatus] = useState('')
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const updatedProject = { projectname, projectdescription, projectdate, projectmanager, projectdeveloper, projectstatus };
-    setIsLoading(true);
-    console.log("Updating project...");
-
-    fetch('http://localhost:8000/projects/' + id, {
-      method: 'PUT',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(updatedProject)
-    })
-    .then(() => {
-      setIsLoading(false)
-    })
+  // I'm not sure what kind of validation you guys wanted, so I simply validated that the fields were not empty.
+  function validateField(value) {
+    let error;
+    if (value === '') {
+      error = 'Empty field not allowed';
+    }
+    return error;
   }
 
   return (
     <div className="App-project">
-      <Formik>
-        <Form className="App-project--form" onSubmit={handleSubmit}>
-          <label className="App-project--form__label" htmlFor="projectName">Project Name</label>
-          <Field className="App-project--form__input" id="projectName" name="projectName" onChange={(e) => setProjectName(e.target.value)}/>
-
-          <label className="App-project--form__label" htmlFor="projectDescription">Description</label>
-          <Field className="App-project--form__input" id="projectDescription" name="projectDescription" onChange={(e) => setProjectDescription(e.target.value)}/>
-
-          <label className="App-project--form__label" htmlFor="projectManager">Project manager</label>
-          <Field className="App-project--form__input" as="select" name="projectManager" onChange={(e) => setProjectManager(e.target.value)}>
-            <option value="xxxxxx">Select a person</option>
-            <option value="Walt Cosani">Walt Cosani</option>
-            <option value="Ignacio Truffa">Ignacio Truffa</option>
-          </Field>
-
-          <label className="App-project--form__label" htmlFor="projectDeveloper">Assigned to</label>
-          <Field className="App-project--form__input" as="select" name="projectDeveloper" onChange={(e) => setProjectDeverloper(e.target.value)}>
-            <option value="xxxxxx">Select a person</option>
-            <option value="Walt Cosani">Walt Cosani</option>
-            <option value="Ignacio Truffa">Ignacio Truffa</option>
-          </Field>
-
-          <label className="App-project--form__label" htmlFor="projectStatus">Status</label>
-          <Field className="App-project--form__input" as="select" name="projectStatus" onChange={(e) => setProjectStatus(e.target.value)}>
-            <option value="enabled">Enabled</option>
-            <option value="disabled">Disabled</option>
-          </Field>
-
-          { error && <div>{ error }</div> }
-          { !isLoading && <button className="Button-Big" type="submit">Save changes</button> }
-          { isLoading && <button className="Button-Big">Saving changes...</button> }
-        </Form>
+      <Formik
+        initialValues={{
+          projectname: '',
+          projectdescription: '',
+          projectdate: 'Creation date:  09/09/2020 10:30 am',
+          projectmanager: '',
+          projectdeveloper: '',
+          projectstatus: 'enabled',
+        }}
+        onSubmit={values => {
+          let updatedValues = values;
+          // console.log(test);
+          setIsLoading(true);
+          console.log("Updating project...");
+          fetch('http://localhost:8000/projects/' + id, {
+            method: 'PUT',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(updatedValues)
+          })
+        }}
+      >
+        {({ errors, touched, isValidating }) => (
+          <Form className="App-project--form">
+            {/* Project name */}
+            <label className="App-project--form__label" htmlFor="projectname">
+              <p>Project Name</p>
+              {errors.projectname && touched.projectname && <div className="App-project--form__error-message">{errors.projectname}</div>}
+            </label>
+            <Field className="App-project--form__input" name="projectname" validate={validateField}/>
+            {/* Project description */}
+            <label className="App-project--form__label" htmlFor="projectdescription">
+              <p>Description</p>
+              {errors.projectdescription && touched.projectdescription && <div className="App-project--form__error-message">{errors.projectdescription}</div>}
+            </label>
+            <Field className="App-project--form__input" name="projectdescription" validate={validateField}/>
+            {/* Project manager */}
+            <label className="App-project--form__label" htmlFor="projectmanager">
+              <p>Project manager</p>
+              {errors.projectmanager && touched.projectmanager && <div className="App-project--form__error-message">{errors.projectmanager}</div>}
+            </label>
+            <Field className="App-project--form__input" as="select" name="projectmanager" validate={validateField}>
+              <option value="xxxxxx">Select a person</option>
+              <option value="Walt Cosani">Walt Cosani</option>
+              <option value="Ignacio Truffa">Ignacio Truffa</option>
+            </Field>
+            {/* Project developer */}
+            <label className="App-project--form__label" htmlFor="projectdeveloper">
+              <p>Assigned to</p>
+              {errors.projectdeveloper && touched.projectdeveloper && <div className="App-project--form__error-message">{errors.projectdeveloper}</div>}
+            </label>
+            <Field className="App-project--form__input" as="select" name="projectdeveloper" validate={validateField}>
+              <option value="xxxxxx">Select a person</option>
+              <option value="Walt Cosani">Walt Cosani</option>
+              <option value="Ignacio Truffa">Ignacio Truffa</option>
+            </Field>
+            {/* Project status */}
+            <label className="App-project--form__label" htmlFor="projectstatus">
+              <p>Status</p>
+              {/* There's no need to use a validator here because the default option ("enabled") will be populated from the start */}
+            </label>
+            <Field className="App-project--form__input" as="select" name="projectstatus">
+              <option value="enabled">Enabled</option>
+              <option value="disabled">Disabled</option>
+            </Field>
+            {/* Submit button */}
+            { error && <div>{ error }</div> }
+            { !isLoading && <button className="Button-Big" type="submit">Save changes</button> }
+            { isLoading && <button className="Button-Big">Saving changes...</button> }
+          </Form>
+        )}
       </Formik>
     </div>
   );
--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -3,60 +3,88 @@ import { Formik, Field, Form } from 'formik';
 import { useState } from "react";
 
 const NewProject = () => {
-  const [projectname, setProjectName] = useState('');
-  const [projectdescription, setProjectDescription] = useState('');
-  const [projectdate] = useState('Creation date:  09/09/2020 10:30 am');
-  const [projectmanager, setProjectManager] = useState('');
-  const [projectdeveloper, setProjectDeverloper] = useState('');
-  const [projectstatus, setProjectStatus] = useState('')
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const project = { projectname, projectdescription, projectdate, projectmanager, projectdeveloper, projectstatus };
-    setIsLoading(true);
-    console.log("Creating project...");
-
-    fetch('http://localhost:8000/projects', {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(project)
-    })
+  // I'm not sure what kind of validation you guys wanted, so I simply validated that the fields were not empty.
+  function validateField(value) {
+    let error;
+    if (value === '') {
+      error = 'Empty field not allowed';
+    }
+    return error;
   }
 
   return (
     <div className="App-project">
-      <Formik>
-        <Form className="App-project--form" onSubmit={handleSubmit}>
-          <label className="App-project--form__label" htmlFor="projectName">Project Name</label>
-          <Field className="App-project--form__input" id="projectName" name="projectName" onChange={(e) => setProjectName(e.target.value)}/>
-
-          <label className="App-project--form__label" htmlFor="projectDescription">Description</label>
-          <Field className="App-project--form__input" id="projectDescription" name="projectDescription" onChange={(e) => setProjectDescription(e.target.value)}/>
-
-          <label className="App-project--form__label" htmlFor="projectManager">Project manager</label>
-          <Field className="App-project--form__input" as="select" name="projectManager" onChange={(e) => setProjectManager(e.target.value)}>
+      <Formik
+        initialValues={{
+          projectname: '',
+          projectdescription: '',
+          projectdate: 'Creation date:  09/09/2020 10:30 am',
+          projectmanager: '',
+          projectdeveloper: '',
+          projectstatus: 'enabled',
+        }}
+        onSubmit={values => {
+          let newValues = values;
+          // console.log(test);
+          setIsLoading(true);
+          console.log("Creating new project...")
+          fetch('http://localhost:8000/projects', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newValues)
+          })
+        }}
+      >
+      {({ errors, touched, isValidating }) => (
+        <Form className="App-project--form">
+          {/* Project name */}
+          <label className="App-project--form__label" htmlFor="projectname">
+            <p>Project Name</p>
+            {errors.projectname && touched.projectname && <div className="App-project--form__error-message">{errors.projectname}</div>}
+          </label>
+          <Field className="App-project--form__input" name="projectname" validate={validateField}/>
+          {/* Project description */}
+          <label className="App-project--form__label" htmlFor="projectdescription">
+            <p>Description</p>
+            {errors.projectdescription && touched.projectdescription && <div className="App-project--form__error-message">{errors.projectdescription}</div>}
+          </label>
+          <Field className="App-project--form__input" name="projectdescription" validate={validateField}/>
+          {/* Project manager */}
+          <label className="App-project--form__label" htmlFor="projectmanager">
+            <p>Project manager</p>
+            {errors.projectmanager && touched.projectmanager && <div className="App-project--form__error-message">{errors.projectmanager}</div>}
+          </label>
+          <Field className="App-project--form__input" as="select" name="projectmanager" validate={validateField}>
             <option value="xxxxxx">Select a person</option>
             <option value="Walt Cosani">Walt Cosani</option>
             <option value="Ignacio Truffa">Ignacio Truffa</option>
           </Field>
-
-          <label className="App-project--form__label" htmlFor="projectDeveloper">Assigned to</label>
-          <Field className="App-project--form__input" as="select" name="projectDeveloper" onChange={(e) => setProjectDeverloper(e.target.value)}>
+          {/* Project developer */}
+          <label className="App-project--form__label" htmlFor="projectdeveloper">
+            <p>Assigned to</p>
+            {errors.projectdeveloper && touched.projectdeveloper && <div className="App-project--form__error-message">{errors.projectdeveloper}</div>}
+          </label>
+          <Field className="App-project--form__input" as="select" name="projectdeveloper" validate={validateField}>
             <option value="xxxxxx">Select a person</option>
             <option value="Walt Cosani">Walt Cosani</option>
             <option value="Ignacio Truffa">Ignacio Truffa</option>
           </Field>
-
-          <label className="App-project--form__label" htmlFor="projectStatus">Status</label>
-          <Field className="App-project--form__input" as="select" name="projectStatus" onChange={(e) => setProjectStatus(e.target.value)}>
+          {/* Project status */}
+          <label className="App-project--form__label" htmlFor="projectstatus">
+            <p>Status</p>
+            {/* There's no need to use a validator here because the default option ("enabled") will be populated from the start */}
+          </label>
+          <Field className="App-project--form__input" as="select" name="projectstatus">
             <option value="enabled">Enabled</option>
             <option value="disabled">Disabled</option>
           </Field>
-
+          {/* Submit button */}
           { !isLoading && <button className="Button-Big" type="submit">Create project</button> }
           { isLoading && <button className="Button-Big">Creating project...</button> }
         </Form>
+      )}
       </Formik>
     </div>
   );
